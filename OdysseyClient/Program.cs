56c8aaf440c428f3cba647de85b1e9c84e0207c3
Luis k Mora <luisk9759@hotmail.com>
2018-05-10 16:31:01 -0600

    creacion interfaz

--- OdysseyClient/Program.cs
+++ OdysseyClient/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace OdysseyClient
 {
